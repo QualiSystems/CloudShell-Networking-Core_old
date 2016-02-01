b3dd46d9da5511ea549ef18ca65bd993cf954a12
Boris Modylevsky <borismod@gmail.com>
2016-02-01 13:32:50 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+setup(name='CloudShell-Networking-Core',
+      version='1.1',
+      description='CloudShell Networking Core functionality',
+      long_description=readme(),
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: System :: Distributed Computing',
+          'Operating System :: Microsoft :: Windows',
+          'Intended Audience :: Information Technology',
+          'Operating System :: MacOS',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: POSIX',
+          'Operating System :: Unix'
+      ],
+      keywords='cloud QualiSystems',
+      url='https://github.com/QualiSystems/CloudShell-Core',
+      author='Boris Modylevsky',
+      author_email='borismod@gmail.com',
+      license='Apache 2.0',
+      packages=['CloudShell-Core'],
+      install_requires=[],
+      test_suite='',
+      tests_require=[],
+      entry_points={},
+      include_package_data=True,
+      zip_safe=False)
